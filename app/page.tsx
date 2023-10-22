0d090f365f0a93020c48368c90ac7cf9ab1a0359
Barna Tamas <barnatamas@gmail.com>
2023-10-22 23:24:32 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By bájbáj central 1{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
